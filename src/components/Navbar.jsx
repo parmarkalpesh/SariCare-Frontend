e6fb4820f4b2788fabd79face6cb8b73ad7d890c
kalpeshparmar <parmarkalpesh1586@gmail.com>
2026-01-07 23:07:52 +0530

    0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ export default function Navbar() {
                         />
                     </div>
                     <span className="text-2xl font-serif font-bold text-gray-900 tracking-tight">
-                        Sari<span className="text-primary">Care</span>
+                        Sari0<span className="text-primary">Care</span>
                     </span>
                 </Link>
 
